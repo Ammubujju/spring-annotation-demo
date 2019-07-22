37368cde3ccedda4792b5cddb245aa4e36d3b860
Ammubujju <51942968+Ammubujju@users.noreply.github.com>
2019-07-22 12:55:41 +0530

    Update BeanPostProcessorDemoBean.java

--- src/main/java/com/stackroute/domain/BeanPostProcessorDemoBean.java
+++ src/main/java/com/stackroute/domain/BeanPostProcessorDemoBean.java
@@ -2,16 +2,17 @@ package com.stackroute.domain;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
-
+//implementing beanpostprocessor
 public class BeanPostProcessorDemoBean implements BeanPostProcessor
 {
+//before initialization
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException
     {
         System.out.println("Post Process After Initialization");
         return null;
     }
-
+//after initialization
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException
     {
