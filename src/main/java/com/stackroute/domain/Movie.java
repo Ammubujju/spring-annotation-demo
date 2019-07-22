71cdc93a88f49019700051de7183aa130180e944
Ammubujju <51942968+Ammubujju@users.noreply.github.com>
2019-07-22 12:51:31 +0530

    Update Movie.java

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -2,7 +2,7 @@ package com.stackroute.domain;
 import com.stackroute.domain.Actor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
+//creating object for actor class 
 public class Movie
 {
     @Autowired
@@ -12,12 +12,12 @@ public class Movie
     public Movie()
     {
     }
-
+//using getter
     public Actor getActor()
     {
         return actor;
     }
-
+//using setter
     @Autowired
     public void setActor(Actor actor)
     {
@@ -28,7 +28,7 @@ public class Movie
     {
         this.actor = actor;
     }
-
+//displaying information
     public void display()
     {
         System.out.println("Actor information : "+actor.getName()+" : gender is :" +actor.getGender()+" : age is :" +actor.getAge());
